827355274d4976d9c8d09ac4634e032321518df1
Andres Munevar <afmunevarm@hotmail.com>
2023-08-22 21:38:07 -0400

    Remove manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,18 +39,6 @@ module.exports = {
         head: true,
       },
     },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.defaultTitle,
-        short_name: 'starter',
-        start_url: '/',
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'minimal-ui',
-        icon: './static/favicon/avataaars.png',
-      },
-    },
     'gatsby-plugin-offline',
   ],
 };
